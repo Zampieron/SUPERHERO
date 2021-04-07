f0f35e7420bfd62bcce3ad376cfb10a5cf2304a1
Zampieron <77388884+Zampieron@users.noreply.github.com>
2021-04-07 09:16:48 +0200

    muovimenti

--- main.ts
+++ main.ts
@@ -1,6 +1,158 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     HERO.vy = -100
 })
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    HERO,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . f 2 f e e e e f f . . . 
+        . . . f 2 2 2 f e e e e f f . . 
+        . . . f e e e e f f e e e f . . 
+        . . f e 2 2 2 2 e e f f f f . . 
+        . . f 2 e f f f f 2 2 2 e f . . 
+        . . f f f e e e f f f f f f f . 
+        . . f e e 4 4 f b e 4 4 e f f . 
+        . . f f e d d f 1 4 d 4 e e f . 
+        . f d d f d d d d 4 e e e f . . 
+        . f b b f e e e 4 e e f . . . . 
+        . f b b e d d 4 2 2 2 f . . . . 
+        . . f b e d d e 4 4 4 f f . . . 
+        . . . f f e e f f f f f f . . . 
+        . . . . f f f . . . f f . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . f 2 f e e e e f f . . . 
+        . . . f 2 2 2 f e e e e f f . . 
+        . . . f e e e e f f e e e f . . 
+        . . f e 2 2 2 2 e e f f f f . . 
+        . . f 2 e f f f f 2 2 2 e f . . 
+        . . f f f e e e f f f f f f f . 
+        . . f e e 4 4 f b e 4 4 e f f . 
+        . . . f e d d f 1 4 d 4 e e f . 
+        . . . . f d d d e e e e e f . . 
+        . . . . f e 4 e d d 4 f . . . . 
+        . . . . f 2 2 e d d e f . . . . 
+        . . . f f 5 5 f e e f f f . . . 
+        . . . f f f f f f f f f f . . . 
+        . . . . f f f . . . f f . . . . 
+        `,img`
+        . . . . . f f f f f f . . . . . 
+        . . . . f 2 f e e e e f f . . . 
+        . . . f 2 2 2 f e e e e f f . . 
+        . . . f e e e e f f e e e f . . 
+        . . f e 2 2 2 2 e e f f f f . . 
+        . . f 2 e f f f f 2 2 2 e f . . 
+        . . f f f e e e f f f f f f f . 
+        . . f e e 4 4 f b e 4 4 e f f . 
+        . . f f e d d f 1 4 d 4 e e f . 
+        . f d d f d d d d 4 e e e f . . 
+        . f b b f e e e 4 e e f f . . . 
+        . f b b e d d 4 2 2 2 f . . . . 
+        . . f b e d d e 2 2 2 e . . . . 
+        . . . f f e e f 4 4 4 f . . . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . . . f f f . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . f 2 f e e e e f f . . . . 
+        . . f 2 2 2 f e e e e f f . . . 
+        . . f e e e e f f e e e f . . . 
+        . f e 2 2 2 2 e e f f f f . . . 
+        . f 2 e f f f f 2 2 2 e f . . . 
+        . f f f e e e f f f f f f f . . 
+        . f e e 4 4 f b e 4 4 e f f . . 
+        . . f e d d f 1 4 d 4 e e f . . 
+        . . . f d d d e e e e e f . . . 
+        . . . f e 4 e d d 4 f . . . . . 
+        . . . f 2 2 e d d e f . . . . . 
+        . . f f 5 5 f e e f f f . . . . 
+        . . f f f f f f f f f f . . . . 
+        . . . f f f . . . f f . . . . . 
+        `],
+    100,
+    true
+    )
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    HERO,
+    [img`
+        . . . . . . f f f f f f . . . . 
+        . . . . f f e e e e f 2 f . . . 
+        . . . f f e e e e f 2 2 2 f . . 
+        . . . f e e e f f e e e e f . . 
+        . . . f f f f e e 2 2 2 2 e f . 
+        . . . f e 2 2 2 f f f f e 2 f . 
+        . . f f f f f f f e e e f f f . 
+        . . f f e 4 4 e b f 4 4 e e f . 
+        . . f e e 4 d 4 1 f d d e f . . 
+        . . . f e e e 4 d d d d f . . . 
+        . . . . f f e e 4 4 4 e f . . . 
+        . . . . . 4 d d e 2 2 2 f . . . 
+        . . . . . e d d e 2 2 2 f . . . 
+        . . . . . f e e f 4 5 5 f . . . 
+        . . . . . . f f f f f f . . . . 
+        . . . . . . . f f f . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f f f . . . . 
+        . . . . f f e e e e f 2 f . . . 
+        . . . f f e e e e f 2 2 2 f . . 
+        . . . f e e e f f e e e e f . . 
+        . . . f f f f e e 2 2 2 2 e f . 
+        . . . f e 2 2 2 f f f f e 2 f . 
+        . . f f f f f f f e e e f f f . 
+        . . f f e 4 4 e b f 4 4 e e f . 
+        . . f e e 4 d 4 1 f d d e f . . 
+        . . . f e e e e e d d d f . . . 
+        . . . . . f 4 d d e 4 e f . . . 
+        . . . . . f e d d e 2 2 f . . . 
+        . . . . f f f e e f 5 5 f f . . 
+        . . . . f f f f f f f f f f . . 
+        . . . . . f f . . . f f f . . . 
+        `,img`
+        . . . . . . f f f f f f . . . . 
+        . . . . f f e e e e f 2 f . . . 
+        . . . f f e e e e f 2 2 2 f . . 
+        . . . f e e e f f e e e e f . . 
+        . . . f f f f e e 2 2 2 2 e f . 
+        . . . f e 2 2 2 f f f f e 2 f . 
+        . . f f f f f f f e e e f f f . 
+        . . f f e 4 4 e b f 4 4 e e f . 
+        . . f e e 4 d 4 1 f d d e f . . 
+        . . . f e e e 4 d d d d f . . . 
+        . . . . f f e e 4 4 4 e f . . . 
+        . . . . . 4 d d e 2 2 2 f . . . 
+        . . . . . e d d e 2 2 2 f . . . 
+        . . . . . f e e f 4 5 5 f . . . 
+        . . . . . . f f f f f f . . . . 
+        . . . . . . . f f f . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f f f . . . . 
+        . . . . f f e e e e f 2 f . . . 
+        . . . f f e e e e f 2 2 2 f . . 
+        . . . f e e e f f e e e e f . . 
+        . . . f f f f e e 2 2 2 2 e f . 
+        . . . f e 2 2 2 f f f f e 2 f . 
+        . . f f f f f f f e e e f f f . 
+        . . f f e 4 4 e b f 4 4 e e f . 
+        . . f e e 4 d 4 1 f d d e f . . 
+        . . . f e e e 4 d d d d f . . . 
+        . . . . 4 d d e 4 4 4 e f . . . 
+        . . . . e d d e 2 2 2 2 f . . . 
+        . . . . f e e f 4 4 5 5 f f . . 
+        . . . . f f f f f f f f f f . . 
+        . . . . . f f . . . f f f . . . 
+        `],
+    100,
+    true
+    )
+})
 let HERO: Sprite = null
 scene.setBackgroundColor(9)
 HERO = sprites.create(img`
